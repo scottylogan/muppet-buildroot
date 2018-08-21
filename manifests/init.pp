2f7ba997f49688a982a4b791091640ebf69dd97b
Scotty Logan <swl@stanford.edu>
2018-08-21 14:31:33 -0700

    only installing cloud-init during AMI builds

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,23 +14,8 @@
 #
 # Copyright (c) 2018 Scotty Logan
 #
-class buildroot (
-  $add_cloud_config
-) {
+class buildroot {
 
   include buildroot::packages
-
-  if ($add_cloud_config) {
-
-    file { '/etc/cloud/cloud.cfg':
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      source  => "puppet:///modules/${module_name}/cloud.cfg",
-      require => Package['cloud-init'],
-    }
-
-  }
 
 }
